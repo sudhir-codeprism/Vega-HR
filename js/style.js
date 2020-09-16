55f435d528be232e59921b945c97be5d38afcb0e
Sudhir Kumar <37929232+sudhir-codeprism@users.noreply.github.com>
2020-09-16 10:05:10 +0530

    JS Pop Up Added

--- js/style.js
+++ js/style.js
@@ -8,4 +8,43 @@
      }
     }
 
-    
+    var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var modal = document.getElementById("myModal1");
+
+var btn = document.getElementById("contactForm");
+
+var span = document.getElementsByClassName("close1")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
